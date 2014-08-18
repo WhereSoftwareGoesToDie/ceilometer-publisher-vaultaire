4bfe6d88a2ba9789457c03050242bd7faaeeada4
Sharif Olorin <sio@tesser.org>
2014-08-18 11:19:31 +1000

    Put version in setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@ import os
 from setuptools import setup
 from pip.req import parse_requirements
 
-with open('VERSION', 'r') as f:
-    VERSION = f.readline().strip()
-
 # Utility function to read the README file.  Used for the long_description.
 # It's nice, because now 1) we have a top level README file and 2) it's easier
 # to type in the README file than to put a raw string in below.
@@ -12,10 +9,9 @@ def read(fname):
     with open(os.path.join(os.path.dirname(__file__), fname)) as f:
         return f.read()
 
-
 setup(
     name="ceilometer-publisher-vaultaire",
-    version=VERSION,
+    version="0.0.4",
     description="A publisher plugin for Ceilometer that outputs to Vaultaire",
     author="Barney Desmond",
     author_email="engineering@anchor.net.au",
