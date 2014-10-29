981635129bea06b0b7aaaa1ed4f88735ddeaf000
Oswyn Brent <oswyn.brent@anchor.com.au>
2014-10-29 11:55:36 +1100

    cleanup

--- ceilometer_publisher_vaultaire/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-from ceilometer_publisher_vaultaire.process import *
-from ceilometer_publisher_vaultaire.vaultaire import VaultairePublisher
-from ceilometer_publisher_vaultaire.siphash import SipHash24
-
-__all__ = ['VaultairePublisher']
